e12e9e6b0ea399b00d24bac5a513c3cc9fc1d380
Brendan Huynh <bhuynh3@huskers.unl.edu>
2020-08-19 01:00:08 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
  * Author: Brendan Huynh
  * Date:   2020
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
@@ -10,6 +10,7 @@
 int main(int argc, char **argv) {
 
   printf("Brendan Huynh!\n");
+  printf("Computer Sciene\n");
 
   return 0;
 }
