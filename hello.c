160759682212502172ac266fa7d52ec9357eb445
Brendan Huynh <bhuynh3@huskers.unl.edu>
2020-08-19 00:45:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Brendan Huynh
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brendan Huynh!\n");
+
+  return 0;
+}
